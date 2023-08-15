0281f95938fb737f302e11ff55b8e4efde69277f
RyanTF313 <r.tillman.french@gmail.com>
2023-08-15 09:04:54 -0400

    high low game

--- index.js
+++ index.js
@@ -1,182 +1,170 @@
-const message = document.getElementById("message");
 const start = document.getElementById("start");
-const gameIdHeading = document.getElementById("game-id");
-// const place = ["start", "draw", "discard", "next"];
-const over = false;
-let deck_id; // 'c4d2sumxr5xq' // get deck id from data fetch
-// const deal = `https://deckofcardsapi.com/api/deck/${deck_id}/pile/${player}/add/?cards=${hand}`
-// const draw = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
-// const discard = `https://deckofcardsapi.com/api/deck/${deck_id}/pile/discard/add/?cards=${card}`
-const userHand = [];
-const compHand = [];
-let userCards = [];
-let compCards = [];
-const user = document.querySelector("#user-hand ul");
-const comp = document.querySelector("#comp-hand ul");
-const deck = document.getElementById("deck");
-const turn = true;
+const restart = document.getElementById("restart");
+restart.style.display = "none";
+const user = document.querySelector("#user");
+const comp = document.querySelector("#comp");
+const reveal = document.getElementById("reveal");
+const gameModeOptions = document.querySelectorAll(".game-type");
+let game;
+
+class HighLow {
+  constructor() {
+    this.userCard = {};
+    this.compCard = {};
+    this.winner = "";
+    this.gameOver = "";
+    this.gameId = "";
+    this.gameType = "";
+    this.message = "";
+    this.rounds = [];
+  }
+
+  startGame = () => {
+    return fetch(
+      "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const { deck_id } = data;
+        this.gameId = deck_id;
+        return data;
+      });
+  };
+
+  dealCards = () => {
+    return fetch(
+      `https://www.deckofcardsapi.com/api/deck/${this.gameId}/draw/?count=2`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const { cards } = data;
+        this.userCard = cards[0];
+        this.compCard = cards[1];
+        user.innerHTML =
+          "<img src='https://www.deckofcardsapi.com/static/img/back.png' />";
+        comp.innerHTML =
+          "<img src='https://www.deckofcardsapi.com/static/img/back.png' />";
+        return data;
+      });
+  };
+
+  revealCards = () => {
+    user.innerHTML = `<img src="${this.userCard.image}" />`;
+    comp.innerHTML = `<img src="${this.compCard.image}" />`;
+    this.checkWin();
+  };
+
+  checkWin = () => {
+    const winLogic = {
+      high: this.checkHigh,
+      low: this.checkLow,
+    };
+
+    winLogic[this.gameType]();
+    this.message =
+      this.winner +
+      " won round " +
+      (this.rounds.length + 1) +
+      "! Please press Quit/Re-Deal to keep playing.";
+
+    this.rounds.push(this.winner);
+    if (this.rounds.length >= 3) {
+      let count = 0;
+      let compCount = 0;
+      let tieCount = 0;
+      this.rounds.forEach((winner) => {
+        if (winner === "You") {
+          count++;
+        } else if (winner === "Tie") {
+          tieCount++;
+        } else {
+          compCount++;
+        }
+      });
+
+      if (count > compCount && count > tieCount) {
+        this.winner = "You";
+        this.gameOver = true;
+        this.message = " You won the series!";
+      } else if (count < compCount && compCount > tieCount) {
+        this.winner = "Computer";
+        this.gameOver = true;
+        this.message = " Computer won the series!";
+      } else if (compCount < tieCount && count < tieCount) {
+        this.winner = "Tie";
+        this.gameOver = true;
+        this.message = " There are mostly Ties. No one won the series!";
+      }
+    }
+    document.getElementById("message").innerText = this.message;
+  };
+
+  checkHigh = () => {
+    if (this.userCard.value === this.compCard.value) {
+      this.winner = "Tie";
+    } else if (this.userCard.value > this.compCard.value) {
+      this.winner = "You";
+    } else {
+      this.winner = "Computer";
+    }
+    return this.userCard.value > this.compCard.value;
+  };
+  checkLow = () => {
+    if (this.userCard.value === this.compCard.value) {
+      this.winner = "Tie";
+    } else if (this.userCard.value < this.compCard.value) {
+      this.winner = "You";
+    } else {
+      this.winner = "Computer";
+    }
+  };
+}
 
 start.addEventListener("click", (e) => {
   e.preventDefault();
-  // get a deck from api
-  startGame();
-  message.innerText = "Let the games begin! User it's your turn";
-  deck.innerHTML =
-    "<img id='backOfDeckImg' src='./card-back-black.png' alt='deck' />";
+  if (game.gameOver) return;
+  if (!game.gameType) return;
+  e.target.style.display = "none";
+  restart.style.display = "flex";
+  game.dealCards();
 });
 
-let startGame = () => {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
+restart.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!game.gameType) return;
+  game.winner = "";
+  game.message = "";
+  game.userCard = {};
+  game.compCard = {};
+  user.innerHTML =
+    "<img src='https://www.deckofcardsapi.com/static/img/back.png' />";
+  comp.innerHTML =
+    "<img src='https://www.deckofcardsapi.com/static/img/back.png' />";
+  game.dealCards();
+});
+
+reveal.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (this.gameOver) return;
+  game.revealCards();
+});
+
+gameModeOptions.forEach((option) =>
+  option.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (this.gameOver) return;
+    if (
+      Array.from(gameModeOptions).find((option) =>
+        option.classList.contains("active")
+      )
+    ) {
+      Array.from(gameModeOptions)
+        .find((option) => option.classList.contains("active"))
+        .classList.remove("active");
+    }
+    game = new HighLow();
+    game.startGame();
+    e.target.classList.add("active");
+    game.gameType = e.target.innerText.toLowerCase();
   })
-    .then((response) => {
-      if (response.ok) return response.json();
-    })
-    .then((data) => {
-      deck_id = data.deck_id;
-      return data;
-    })
-    .then((data) => {
-      // draw a card to send to discard pile
-      // drawCard(data)
-      const { deck_id } = data;
-      gameIdHeading.innerText += " " + deck_id;
-      createhands(deck_id);
-    });
-};
-
-let createhands = (deck) => {
-  fetch(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=10`, {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => {
-      if (response.ok) return response.json();
-    })
-    .then((data) => {
-      // start discard pile
-      //   startDiscard(data.deck_id, data.cards[0].code);
-      const { cards } = data;
-      cards.forEach((card, i) => {
-        if (i % 2 === 0 || i === 0) {
-          userHand.push(card);
-        } else {
-          compHand.push(card);
-        }
-      });
-      user.innerHTML += userHand
-        .map(
-          (card) => `<li class="card">
-      <img src=${card.image} />
-      <input type="hidden" data-card-value="${card.value}" data-card-suit="${card.suit}" data-card-code="${card.code}"/>
-      </li>`
-        )
-        .join("");
-      comp.innerHTML += [...Array(5)]
-        .map(
-          () => `<li class="card">
-      <img src="./card-back-black.png" />
-      </li>`
-        )
-        .join("");
-      document
-        .querySelectorAll("#user-hand li")
-        .forEach((card) => card.addEventListener("click", selectCard));
-    });
-};
-let shuffleDeck = (deck) => {
-  fetch(`https://deckofcardsapi.com/api/deck/${deck}/shuffle/`, {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => {
-      if (response.ok) return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-};
-
-let drawOneCard = () => {
-  fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`, {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((response) => {
-      if (response.ok) return response.json();
-    })
-    .then((data) => {
-      // start discard pile
-    //   startDiscard(data.deck_id, data.cards[0].code);
-    console.log(data)
-    });
-};
-// let startDiscard = (deck, cards) => {
-//   fetch(
-//     `https://deckofcardsapi.com/api/deck/${deck}/pile/discard/add/?cards=${cards}`,
-//     {
-//       method: "get",
-//       headers: { "Content-Type": "application/json" },
-//     }
-//   )
-//     .then((response) => {
-//       if (response.ok) return response.json();
-//     })
-//     .then((data) => {
-//       getLastDiscard(data.deck_id);
-//     });
-// };
-// let getLastDiscard = (deck) => {
-//   fetch(`https://deckofcardsapi.com/api/deck/${deck}/pile/discard/list/`, {
-//     method: "get",
-//     headers: { "Content-Type": "application/json" },
-//   })
-//     .then((response) => {
-//       if (response.ok) return response.json();
-//     })
-//     .then((data) => {
-//       // console.log(data);
-//       let cards = data.piles.discard.cards;
-//       document.getElementById("discard").src = cards[cards.length - 1].image;
-//       start.style.display = "none";
-//       // createHand(data.deck_id)
-//     });
-// };
-
-// let createHand = (deck, cards, player) => {
-//   fetch(
-//     `https://deckofcardsapi.com/api/deck/${deck}/pile/${player}/add/?cards=${cards}`,
-//     {
-//       method: "get",
-//       headers: { "Content-Type": "application/json" },
-//     }
-//   )
-//     .then((response) => {
-//       if (response.ok) return response.json();
-//     })
-//     .then((data) => {
-//       // console.log(data);
-//       // let cards = data.piles.discard.cards
-//       // document.getElementById('discard').src = cards[cards.length - 1].image
-//       // createHand(data.deck_id)
-//     });
-// };
-
-const selectCard = (e) => {
-  const img = e.target;
-  const dataFrmInput = e.target.parentNode.childNodes[3].dataset;
-
-  if (img.style.border === "") {
-    img.style.border = "3px solid blue";
-    userCards.push(dataFrmInput);
-  } else {
-    img.style.border = "";
-    userCards = [
-      ...userCards.filter(
-        (card) => card.cardCode !== dataFrmInput.cardCode
-      ),
-    ];
-  }
-  console.log(img, dataFrmInput, userCards);
-};
+);
